e8172b1f018600f8627e49aa214390339b6f5f2d
hugosenari <hugosenari@gmail.com>
2022-03-12 04:14:51 -0300

    chore: re index crosscompile

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Crosscompile Nim source code into multiple targets on Linux with this proc.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."crosscompile-master".url = "path:./master";
+  inputs."crosscompile-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."crosscompile-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
